58a82b40cf7358862d3640c3cb320690ef08bfe9
danielakujawa <kujawa.daniela@gmail.com>
2018-08-02 17:21:58 +0200

    signup and login working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
+const routes: Routes = [ { path: 'signup', component: SignupPageComponent }, { path: 'login', component: LoginPageComponent }];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupPageComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  feedbackEnabled = false;
+  error = null;
+  processing = false;
+  username: string;
+  password: string;
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  submitForm(form) {
+    this.error = '';
+    this.feedbackEnabled = true;
+    if (form.valid) {
+      this.processing = true;
+      this.authService.login(this.username, this.password)
+        .then(() => {
+         this.router.navigate(['/']);
+        })
+      .catch((err) => {
+        this.error = err.error;
+        this.processing = false;
+        this.feedbackEnabled = false;
+      });
+      }
+  }
+}
