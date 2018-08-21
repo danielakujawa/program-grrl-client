64025fde38f1050208441faccfc6ec5e1fa2e02e
danielakujawa <kujawa.daniela@gmail.com>
2018-08-21 20:51:00 +0200

    prueba

--- src/app/pages/applicants-list/applicants-list.component.ts
+++ src/app/pages/applicants-list/applicants-list.component.ts
@@ -33,7 +33,7 @@ export class ApplicantsListComponent implements OnInit {
 
    }
 
-   submitForm() {
+   submitForm(form) {
 
     this.userService.getFilteredApplicants(this.programmingLanguages)
       .then((result) => {
